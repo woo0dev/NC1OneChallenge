8c74231814316134255ab86b79238c4e2cff6b59
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-06-29 02:18:35 +0900

    RecordVM 쓰지 않는 함수 제거

--- NC1OneChallenge/ViewModel/RecordVM.swift
+++ NC1OneChallenge/ViewModel/RecordVM.swift
@@ -48,45 +48,6 @@ class RecordVM: ObservableObject {
         }
     }
     
-    func fetchMyRecord1(categoryName: String, userUid: String, _ completion: @escaping (_ data: [Record]) -> Void) {
-        var records = [Record(recordUid: "", userUid: "", userName: "", categoryName: "", date: "")]
-        
-        let g = DispatchGroup()
-        g.enter()
-        
-        db.collection("Record").getDocuments() { (querySnapShot, error) in
-            guard let documents = querySnapShot?.documents else {
-                print("No Documents")
-                return
-            }
-            
-            self.myRecord = documents.map({ (queryDocumentSnapshot) -> Record in
-                let data = queryDocumentSnapshot.data()
-                if (data["categoryName"] as? String ?? "") == categoryName && (data["userUid"] as? String ?? "") == userUid {
-                    let recordUid = data["recordUid"] as? String ?? ""
-                    let userUid = data["userUid"] as? String ?? ""
-                    let userName = data["userName"] as? String ?? ""
-                    let categoryName = data["categoryName"] as? String ?? ""
-                    let date = data["date"] as? String ?? ""
-                    return Record(recordUid: recordUid, userUid: userUid, userName: userName, categoryName: categoryName, date: date)
-                } else {
-                    return Record(recordUid: "", userUid: "", userName: "", categoryName: "", date: "")
-                }
-            })
-            
-            g.leave()
-        }
-        
-        records = myRecord.filter { $0.recordUid != "" }
-        
-        g.notify(queue: .main) {
-            completion(records)
-        }
-        
-        return
-        
-    }
-    
     func fetchMyRecord(categoryName: String, userUid: String) async throws {
         let documents = try await db.collection("Record").getDocuments()
         self.myRecord = documents.documents.map({ (queryDocumentSnapshot) -> Record in
@@ -105,8 +66,3 @@ class RecordVM: ObservableObject {
         self.myRecord = self.myRecord.filter { $0.recordUid != "" }
     }
 }
-//func getUserInfo() async throws -> User {
-//    let uid = Auth.auth().currentUser?.uid != nil ? Auth.auth().currentUser!.uid : ""
-//    let data = try await Firestore.firestore().collection("User").document(uid).getDocument().data()
-//    return User(uid: data?["uid"] as? String ?? "", name: data?["name"] as? String ?? "")
-//}
